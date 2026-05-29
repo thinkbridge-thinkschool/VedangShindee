4ebdd46188b0b5ea9c24b7d0d35637b0dc642679
VedangShindee <verekhs@gmail.com>
2026-05-29 14:24:52 +0530

    Remove Day11

--- Day11/piece1/QuotesApi/Telemetry/QuoteActivitySource.cs
+++
@@ -1,9 +0,0 @@
-using System.Diagnostics;
-
-namespace QuotesApi.Telemetry;
-
-public static class QuoteActivitySource
-{
-    public const string Name = "QuotesApi";
-    public static readonly ActivitySource Instance = new(Name, "1.0.0");
-}
